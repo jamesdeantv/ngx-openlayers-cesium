7baf1469313babe9ef664f12fe4ba354b527c027
jamesdeantv@users.noreply.github.com <hey@jamesdean.tv>
2017-12-14 11:49:42 -0500

    Create repo

--- example/src/app/app.component.ts
+++ example/src/app/app.component.ts
@@ -1,32 +1,43 @@
-import {Component} from '@angular/core';
+import { Component, AfterViewInit, QueryList, ViewChildren } from '@angular/core';
+import { olcs } from 'ol-cesium';
+import { MapComponent } from 'ngx-openlayers';
+// import { AolMapComponent } from 'ngx-openlayers';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-    public zoom = 15;
-    public opacity = 1.0;
-    public width = 5;
+export class AppComponent implements AfterViewInit {
+  public zoom = 15;
+  public opacity = 1.0;
+  public width = 5;
+  @ViewChildren(MapComponent) children: QueryList<MapComponent>;
+  ol3d: any;
 
-    increaseZoom() {
-        this.zoom  = Math.min(this.zoom + 1, 18);
-        console.log('zoom: ', this.zoom);
-    }
+  ngAfterViewInit() {
+    console.log('olcs', olcs);
+    this.ol3d = new olcs.OLCesium({map: this.children.first.instance});
+    this.ol3d.setEnabled(true);
+  }
 
-    decreaseZoom() {
-        this.zoom  = Math.max(this.zoom - 1, 1);
-        console.log('zoom: ', this.zoom);
-    }
+  increaseZoom() {
+    this.zoom = Math.min(this.zoom + 1, 18);
+    console.log('zoom: ', this.zoom);
+  }
 
-    increaseOpacity() {
-        this.opacity  = Math.min(this.opacity + 0.1, 1);
-        console.log('opacity: ', this.opacity);
-    }
+  decreaseZoom() {
+    this.zoom = Math.max(this.zoom - 1, 1);
+    console.log('zoom: ', this.zoom);
+  }
 
-    decreaseOpacity() {
-        this.opacity  = Math.max(this.opacity - 0.1, 0);
-        console.log('opacity: ', this.opacity);
-    }
+  increaseOpacity() {
+    this.opacity = Math.min(this.opacity + 0.1, 1);
+    console.log('opacity: ', this.opacity);
+  }
+
+  decreaseOpacity() {
+    this.opacity = Math.max(this.opacity - 0.1, 0);
+    console.log('opacity: ', this.opacity);
+  }
 }
